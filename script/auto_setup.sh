ae1f0b971d2e0eed15c7c246942dcc92d4a2090f
iamhyc <sudofree@163.com>
2017-07-24 22:57:00 +0800

    update auto acript

--- script/auto_setup.sh
+++ script/auto_setup.sh
@@ -4,12 +4,14 @@
 HOSTS='/etc/hosts'
 HOSTS_BK='/etc/hosts.save'
 
+ROS_MASTER='TX1-team05'
+
 # dependency check
 function pkg_check(){
 	pkg=$1;
 	pkg_status=$(dpkg-query -l $pkg | awk '($1=="rc") {print "N"}')
 	if [ -n "$pkg_status" ];then
-		echo '[PENDING]' $pkg
+		echo '[PENDING..]' $pkg
  		sudo apt-get install -y $pkg 1> /dev/null
  		echo '[INSTALLED]' $pkg
  	else
@@ -18,14 +20,23 @@ function pkg_check(){
 }
 echo '----------------Dependency Check Start----------------------'
 pkg_check "samba"
-echo '-----------------Dependency Check Over----------------------'
+echo '----------------Dependency Check Over-----------------------'
+echo
+
+#udev rules release setup
+sudo cp ./car_udev.rules /etc/udev/rules.d/
+
+#service re-config
+sudo service samba restart
 
 #cleanup HOSTS and rebuild
+echo
 echo '-------------------HOST Setup Start-------------------------'
 sudo cp $HOSTS $HOSTS_BK
 sudo echo -e 'localhost\t\t127.0.0.1' > $HOSTS
+sudo echo -e $(hostname)'\t\t127.0.1.1' >> $HOSTS
 
-for ip_address in $(nmap -p 137 192.168.0.0/24 -oG - | grep netbios | awk '{print $2}')
+for ip_address in $(nmap -p 137 192.168.0.0/24 --max-rtt-timeout 5 -oG - | grep netbios | awk '{print $2}')
 do
         hostname=$(timeout 1 nmblookup -A $ip_address | awk '$2 == "<20>" {print $1}')
         if [ -n "$hostname" ];then
@@ -34,13 +45,14 @@ do
 done
 cat $HOSTS
 echo '------------------HOST Setup Over--------------------------'
+echo
 
 #env_var export
+set ROS_MASTER_URI='http://'$ROS_MASTER':11311'
 
 #source workspace setup
-
-#udev rules release setup
-
-#service re-config
+source '/home/'$(who | awk '{print $1}')'/auto_car/devel/setup.'$(ps | grep $$ | awk '{print $4}')
 
 #self-check & launch file selection
+	#print out help and status information
+	#print out checkbox[] selections in while loop
